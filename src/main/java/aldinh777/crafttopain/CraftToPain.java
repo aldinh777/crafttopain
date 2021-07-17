7ad011dc7ecd3bed1fcb298677054180ccac4ac1
aldinh777 <aldi.nh789@gmail.com>
2021-07-17 22:43:09 +0800

    version v1.0.4

--- src/main/java/aldinh777/crafttopain/CraftToPain.java
+++ src/main/java/aldinh777/crafttopain/CraftToPain.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class CraftToPain {
     public static final String MODID = "crafttopain";
     public static final String NAME = "Craft to Pain";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     @Mod.Instance
     public static CraftToPain INSTANCE;
